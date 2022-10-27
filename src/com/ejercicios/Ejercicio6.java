cedbd1405503239b6f5c8a3a525ccb2867cb71e0
Aitor C. Figueroa <aitorcf@gmail.com>
2022-10-27 16:54:35 +0200

    Ejercicio 6 done

---
+++ src/com/ejercicios/Ejercicio6.java
@@ -0,0 +1,37 @@
+package com.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce el primer número:");
+        int num1 = Integer.parseInt(input.nextLine());
+        System.out.println("Selecciona una operación introduciendo su símbolo: \nSuma (+) \nResta (-) \nMultiplicación (*) \nDivisión (/)");
+        String ope = input.nextLine();
+        System.out.println("Introduce el segundo número:");
+        int num2 = Integer.parseInt((input.nextLine()));
+
+        int resultado = 0;
+
+        if (!ope.equals("+") && !ope.equals("-") && !ope.equals("*") && !ope.equals("/")) {
+            System.out.println("El símbolo introducido no se corresponde con una de las posibilidades.");
+        } else {
+            switch (ope) {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+            }
+            System.out.println("El resultado de la operación " + num1 + ope + num2 + " es " + resultado);
+        }
+    }
+}
