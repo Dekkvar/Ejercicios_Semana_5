6d797e0ed9936d5d38dd11512997910db288bdba
Aitor C. Figueroa <aitorcf@gmail.com>
2022-10-27 23:16:56 +0200

    Ejercicio 5 done

--- src/com/ejercicios/Ejercicio5.java
+++ src/com/ejercicios/Ejercicio5.java
@@ -5,16 +5,33 @@ import java.util.Scanner;
 public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int count = 0;
-        int suma = 0;
+        System.out.println("Introduce el primer número:");
+        int num1 = input.nextInt();
+        System.out.println("Selecciona una operación introduciendo su símbolo: \nSuma (+) \nResta (-) \nMultiplicación (*) \nDivisión (/)");
+        String ope = input.nextLine();
+        System.out.println("Introduce el segundo número:");
+        int num2 = input.nextInt();
 
-        for (int i = 0; i < 10; i++) {
-            count++;
-            System.out.println("Introduce un número:");
-            int num = Integer.parseInt(input.nextLine());
-            suma += num;
-            double media = (double)suma / count;
-            System.out.println("La media de " + suma + " es " + media);
+        int resultado = 0;
+
+        if (!ope.equals("+") && !ope.equals("-") && !ope.equals("*") && !ope.equals("/")) {
+            System.out.println("El símbolo introducido no se corresponde con una de las posibilidades.");
+        } else {
+            switch (ope) {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+            }
+            System.out.println("El resultado de la operación " + num1 + ope + num2 + " es " + resultado);
         }
     }
 }
