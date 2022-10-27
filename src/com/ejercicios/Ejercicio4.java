80ccb53562a78f4c44a7e3ba214d54e5cadaf53e
Aitor C. Figueroa <aitorcf@gmail.com>
2022-10-27 23:15:03 +0200

    Ejercicio 4 done

--- src/com/ejercicios/Ejercicio4.java
+++ src/com/ejercicios/Ejercicio4.java
@@ -1,7 +1,20 @@
 package com.ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+        int suma = 0;
 
+        for (int i = 0; i < 10; i++) {
+            count++;
+            System.out.println("Introduce un número:");
+            int num = Integer.parseInt(input.nextLine());
+            suma += num;
+            double media = (double)suma / count;
+            System.out.println("La media de " + suma + " es " + media);
+        }
     }
 }
