d4752ffa2e05b24389c94388d2fcae0ef62012f3
Aitor C. Figueroa <aitorcf@gmail.com>
2022-10-27 23:08:58 +0200

    Ejercicio 1 done

--- src/com/ejercicios/Ejercicio1.java
+++ src/com/ejercicios/Ejercicio1.java
@@ -7,9 +7,9 @@ public class Ejercicio1 {
         System.out.println("Vamos a comprobar si dos número son múltiplos.");
         Scanner input = new Scanner(System.in);
         System.out.println("Introduce un número:");
-        int num1 = Integer.parseInt(input.nextLine());
+        int num1 = input.nextInt();
         System.out.println("Introduce otro número:");
-        int num2 = Integer.parseInt(input.nextLine());
+        int num2 = input.nextInt();
 
         System.out.println(num1 % num2 == 0);
     }
