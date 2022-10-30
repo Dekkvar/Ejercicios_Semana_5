9487c8d8ff9273035997468d0cad08db24e887a5
Aitor C. Figueroa <aitorcf@gmail.com>
2022-10-30 12:52:42 +0100

    Ejercicio 5 done

--- src/com/ejercicios/Ejercicio5.java
+++ src/com/ejercicios/Ejercicio5.java
@@ -4,34 +4,44 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
+        int num1, num2;
+        String ope = "";
         Scanner input = new Scanner(System.in);
+
         System.out.println("Introduce el primer número:");
-        int num1 = input.nextInt();
-        System.out.println("Selecciona una operación introduciendo su símbolo: \nSuma (+) \nResta (-) \nMultiplicación (*) \nDivisión (/)");
-        String ope = input.nextLine();
+        num1 = input.nextInt();
+
+        while (ope.equals("")) {
+            ope = input.nextLine();
+            if (!ope.equals("+") && !ope.equals("-") && !ope.equals("*") && !ope.equals("/")) {
+                if (!ope.equals("")) {
+                    System.out.println("El símbolo introducido no se corresponde con una de las posibilidades.");
+                }
+                System.out.println("Selecciona una operación introduciendo su símbolo: \nSuma (+) \nResta (-) \nMultiplicación (*) \nDivisión (/)");
+                ope = "";
+            }
+        }
+
         System.out.println("Introduce el segundo número:");
-        int num2 = input.nextInt();
+        num2 = input.nextInt();
 
         int resultado = 0;
 
-        if (!ope.equals("+") && !ope.equals("-") && !ope.equals("*") && !ope.equals("/")) {
-            System.out.println("El símbolo introducido no se corresponde con una de las posibilidades.");
-        } else {
-            switch (ope) {
-                case "+":
-                    resultado = num1 + num2;
-                    break;
-                case "-":
-                    resultado = num1 - num2;
-                    break;
-                case "*":
-                    resultado = num1 * num2;
-                    break;
-                case "/":
-                    resultado = num1 / num2;
-                    break;
-            }
-            System.out.println("El resultado de la operación " + num1 + ope + num2 + " es " + resultado);
+        switch (ope) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
         }
+
+        System.out.println("El resultado de la operación " + num1 + ope + num2 + " es " + resultado);
     }
 }
